fd0f5096779e472218a60b8c310705ad02732b11
Peter Johnson <pajohnson@email.wm.edu>
2013-08-12 12:55:30 -0400

    Restructured cron.tab as a factory parsing method.

--- cron/tab.py
+++ cron/tab.py
@@ -33,88 +33,20 @@ class CronTab(object):
 
 class CronTabEntry(object):
 
-    def __init__(self, entry):
+    def __init__(self, entry, fields, command, **kwargs):
         self.entry = entry
-        self.fields = []
-        self.dom_or_dow_star = False
-        self.when_reboot = False
-        self.parse(entry)
+        self.fields = fields
+        self.command = command
+        self.dom_or_dow_star = kwargs.get('dom_or_dow_star', False)
+        self.when_reboot = kwargs.get('when_reboot', False)
 
-    def parse(self, entry):
-
-        # Reboot is a special case.
-        if entry.lower().startswith('@reboot'):
-            self.command = self.entry[7:].lstrip()
-            self.when_reboot = True
-            return
-
-        # Replace predefined time specifiers.
-        if entry.startswith('@'):
-            try:
-                token = entry.split()[0]
-                val = PREDEFINED[token.lower()]
-                entry = entry.replace(token, PREDEFINED[token], 1)
-            except KeyError:
-                mesg = 'bad time specifier: {!r}'.format(val)
-                raise CronTabError, mesg
-
-        # Parse the fields.
-        for expr, field in zip(entry.split(), FIELDS):
-            entry = entry[len(expr):].lstrip()
-            try:
-                bits = self.parse_field(expr.lower(), field)
-                self.fields.append(bits)
-            except ValueError:
-                mesg = 'error parsing field: {!r}'.format(expr)
-                raise CronTabError, mesg
-
-        # The command is whatever is left.
-        self.command = entry.strip()
-
-    def parse_field(self, expr, field):
-        lo, hi, names = FIELD_INFO[field]
-        bits = [False for i in range(hi - lo + 1)]
-
-        # Replace names.
-        if names is not None:
-            for i, name in enumerate(names, lo):
-                if name in expr:
-                    expr = expr.replace(name, str(i))
-
-        # Iterate through comma separated values.
-        for val in expr.split(','):
-            step = 1
-            if '/' in val:
-                # Slash changes the step amount.
-                val, step = val.split('/')
-                step = int(step)
-
-            if val == '*':
-                # Set the DOM/DOW flag.
-                if field in (DOM, DOW):
-                    self.dom_or_dow_star = True
-                # Asterisk means to include all values.
-                start, stop = lo, hi
-            elif '-' in val:
-                # Dash indicates a range of values.
-                start, stop = map(int, v.split('-'))
-            else:
-                # Only a single number, not a range.
-                bits[int(val) - lo] = True
-                continue
-
-            # Set all values in the range.
-            for i in range(start, stop + 1, step):
-                bits[i - lo] = True
-
-        # Both 0 and 7 are Sunday.
-        if field == DOW and (bits[0] or bits[7]):
-            bits[0] = bits[7] = True
-         
-        return bits
-
-    def iter_field(self, field):
+    def iter_field(self, field, cache={}):
         '''Iterate through the matching values for a field.'''
+        if field not in cache:
+            cache[field] = self._iter_field(field)
+        return cache[field]
+
+    def _iter_field(self, field):
         lo = FIELD_INFO[field][0]
         fields = self.fields[field]
         start = 0
@@ -173,3 +105,91 @@ class CronTabEntry(object):
 
             # Try next year.
             year += 1              
+
+
+def parse_entry(entry):
+    '''Parse a single crontab entry.'''
+
+    # Reboot is a special case.
+    if entry.lower().startswith('@reboot'):
+        command = self.entry[7:].lstrip()
+        return CronTabEntry(entry, None, command, when_reboot=True)
+
+    # Replace predefined time specifiers.
+    if entry.startswith('@'):
+        try:
+            token = entry.split()[0]
+            val = PREDEFINED[token.lower()]
+            entry = entry.replace(token, PREDEFINED[token], 1)
+        except KeyError:
+            mesg = 'bad time specifier: {!r}'.format(val)
+            raise CronTabError, mesg
+
+    fields = []
+    command = entry[::]
+    kwargs = {}
+
+    # Parse the fields.
+    for expr, field in zip(entry.split(), FIELDS):
+        command = command[len(expr):].lstrip()
+        try:
+            bits = parse_field(expr.lower(), field, kwargs)
+            fields.append(bits)
+        except ValueError:
+            mesg = 'error parsing field: {!r}'.format(expr)
+            raise CronTabError, mesg
+
+    # The command is whatever is left.
+    command = command.split()
+
+    if len(fields) < len(FIELDS) or not command:
+        mesg = 'error parsing entry {!r}'.format(entry)
+        raise CronTabError, mesg
+
+    return CronTabEntry(entry, fields, command, **kwargs)
+
+
+
+
+def parse_field(expr, field, kwargs):
+    '''Parse a field from a crontab entry.'''
+    lo, hi, names = FIELD_INFO[field]
+    bits = [False for i in range(hi - lo + 1)]
+
+    # Replace names.
+    if names is not None:
+        for i, name in enumerate(names, lo):
+            if name in expr:
+                expr = expr.replace(name, str(i))
+
+    # Iterate through comma separated values.
+    for val in expr.split(','):
+        step = 1
+        if '/' in val:
+            # Slash changes the step amount.
+            val, step = val.split('/')
+            step = int(step)
+
+        if val == '*':
+            # Set the DOM/DOW flag.
+            if field in (DOM, DOW):
+                kwargs['dom_or_dow_star'] = True
+            # Asterisk means to include all values.
+            start, stop = lo, hi
+        elif '-' in val:
+            # Dash indicates a range of values.
+            start, stop = map(int, v.split('-'))
+        else:
+            # Only a single number, not a range.
+            bits[int(val) - lo] = True
+            continue
+
+        # Set all values in the range.
+        for i in range(start, stop + 1, step):
+            bits[i - lo] = True
+
+    # Both 0 and 7 are Sunday.
+    if field == DOW and (bits[0] or bits[7]):
+        bits[0] = bits[7] = True
+     
+    return bits
